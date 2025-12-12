50be94615dac945d19f041d9b0c18cb3c35dbbbd
Log Lab <testesappandroid@gmail.com>
2025-12-12 19:00:38 +0000

    File changes

--- src/components/home/BrandingHeader.jsx
+++ src/components/home/BrandingHeader.jsx
@@ -103,10 +103,13 @@ export default function BrandingHeader() {
                 href="https://guia.folhanewsbrasil.com.br/" 
                 target="_blank"
                 rel="noopener noreferrer"
-                className="btn-guia-folha"
+                className="special-cta-button"
+                title="Acesse o Guia Folha"
               >
-                <MapPin className="icon-guia-pin" />
-                <span>Guia Folha</span>
+                <svg className="cta-icon-pulse" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+                </svg>
+                <span className="cta-text">GUIA FOLHA</span>
               </a>
             </li>
             
